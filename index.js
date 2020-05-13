1c1a197374e53a278adb3eab1396a1c321fdd7ac
tippitytapp <marc.tapp@gmail.com>
2020-05-13 10:31:57 -0400

    server setup and running

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const server = require("./server.js");
-
-const PORT = process.env.PORT || 4000;
+require('dotenv').config();
+const colors = require('colors');
+const NODE = process.env.NODE_ENV
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
-    console.log(`\n** API running on port: ${PORT} **\n`);
+    console.log(`\n** API running in ${NODE} mode on http://localhost:${PORT} **\n`.magenta.underline.bold);
 });
